87073c244e4596632b6738120fc74c633097c85d
Matyas Gonzalez <matyas8623@hotmail.com>
2022-09-20 13:27:52 -0300

    ejercicos openboot intro tema 3

---
+++ src/main/java/com/mycompany/openbootcamp/Coche.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.openbootcamp;
+
+
+public class Coche {
+    int puertas=0;
+    Coche(){
+    }
+   
+   public void agregarP(){
+
+     puertas++;
+        }
+   public void mostrarP(){
+    System.out.println("el numero de puertas es "+puertas);
+}
+}
---
+++ src/main/java/com/mycompany/openbootcamp/OpenBootCamp.java
@@ -0,0 +1,41 @@
+
+
+/*ejercicios tema 3 
+Crear una función con tres parámetros que sean números que se suman entre sí.
+Llamar a la función en el main y darle valores. */
+
+/*package com.mycompany.openbootcamp;
+public class OpenBootCamp {
+
+    public static void main(String[] args) {
+        int num = 6 ;
+        int num2= 9 ;
+        int num3= 5 ;
+        
+        int resultado =suma(num,num2,num3);
+        System.out.println("el resultado es "+resultado);
+       }
+      public static int suma(int a, int b, int c){
+        
+          return a+b+c; 
+      }
+}*/
+
+/*Crear una clase coche.
+Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+Una función que incremente el número de puertas que tiene el coche.
+Crear un objeto miCoche en el main y añadirle una puerta.
+Mostrar el número de puertas que tiene el objeto.*/
+/*package com.mycompany.openbootcamp;
+public class OpenBootCamp {
+
+    public static void main(String[] args) {
+   
+        Coche miCoche = new Coche();
+        miCoche.agregarP();
+        miCoche.mostrarP();
+    }
+}*/
+
+
+
